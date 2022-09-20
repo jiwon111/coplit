5eafc94e45c8d2eeda91f41008fb33c49fc09842
jiwon111 <dreamjiwon@naver.com>
2022-09-20 12:41:05 +0900

    220920

---
+++ Day1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Day1 { 
+	public HashMap<String, String> transformFirstAndLast(String[] arr) {
+		HashMap<String, String> map = new HashMap<>();
+    
+    if (arr.length==0) return null;
+    map.put(arr[0], arr[arr.length-1]);
+    return map;
+	} 
+}
