b54f16d326a9e047ca7e32b81869dd863e9daa74
Kim Jiwon <65504940+jiwon111@users.noreply.github.com>
2022-09-20 12:43:00 +0900

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -4,8 +4,8 @@ public class Day1 {
 	public HashMap<String, String> transformFirstAndLast(String[] arr) {
 		HashMap<String, String> map = new HashMap<>();
     
-    if (arr.length==0) return null;
-    map.put(arr[0], arr[arr.length-1]);
-    return map;
+    		if (arr.length==0) return null;
+    		map.put(arr[0], arr[arr.length-1]);
+    		return map;
 	} 
 }
