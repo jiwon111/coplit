d13551e67846b1e8f1068483ce0024904e471e84
jiwon111 <dreamjiwon@naver.com>
2022-09-22 18:30:38 +0900

    Create Day3.java

--- daily-coding/Day3.java
+++ daily-coding/Day3.java
@@ -1,14 +1,16 @@
-import java.util.*;
-
 public class Day3 {
     public boolean powerOfTwo(long num) {
         long power = 2;
-        if (num==1) return true;
-        if (num%2==1) return false;
-        while(power<num){
-            power*=2;
+        if (num == 1)
+            return true;
+        if (num % 2 == 1)
+            return false;
+        while (power < num) {
+            power *= 2;
         }
-        return power == num;
+        if (power == num)
+            return true;
+        else
+            return false;
     }
 }
-
